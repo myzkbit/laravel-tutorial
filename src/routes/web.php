f3bf56f9c6c54737c0ade562e6297c87ad3ba308
h-miyazaki <umorih1031@gmail.com>
2023-06-12 22:59:33 +0900

    フォルダ一覧画面追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\TaskController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,5 +20,7 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::get('/folders/{id}/tasks', [TaskController::class, 'index'])->name('tasks.index');
 
 require __DIR__.'/auth.php';
