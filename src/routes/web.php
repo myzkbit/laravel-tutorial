8619c6f48fce8aa58e0d5befa7011128fa266e7c
h-miyazaki <umorih1031@gmail.com>
2023-06-13 14:34:27 +0900

    認可機能 実装

--- src/routes/web.php
+++ src/routes/web.php
@@ -18,13 +18,16 @@ use App\Http\Controllers\HomeController;
 Route::group(['middleware' => 'auth'], function() {
     Route::get('/', [HomeController::class, 'index'])->name('home');
 
-    Route::get('/folders/{id}/tasks', [TaskController::class, 'index'])->name('tasks.index');
     Route::get('/folders/create', [FolderController::class, 'showCreateForm'])->name('folders.create');
     Route::post('/folders/create', [FolderController::class, 'create']);
-    Route::get('/folders/{id}/tasks/create', [TaskController::class, 'showCreateForm'])->name('tasks.create');
-    Route::post('/folders/{id}/tasks/create', [TaskController::class, 'create']);
-    Route::get('/folders/{id}/tasks/{task_id}/edit', [TaskController::class, 'showEditForm'])->name('tasks.edit');
-    Route::post('/folders/{id}/tasks/{task_id}/edit', [TaskController::class, 'edit']);
+
+    Route::group(['middleware' => 'can:view,folder'], function() {
+        Route::get('/folders/{folder}/tasks', [TaskController::class, 'index'])->name('tasks.index');
+        Route::get('/folders/{folder}/tasks/create', [TaskController::class, 'showCreateForm'])->name('tasks.create');
+        Route::post('/folders/{folder}/tasks/create', [TaskController::class, 'create']);
+        Route::get('/folders/{folder}/tasks/{task}/edit', [TaskController::class, 'showEditForm'])->name('tasks.edit');
+        Route::post('/folders/{folder}/tasks/{task}/edit', [TaskController::class, 'edit']);
+    });
 });
 
 require __DIR__.'/auth.php';
